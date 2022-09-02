dc791ef9f9177063ed9795aec9b5b8305d8be56a
Emma Walker <walkere@tcd.ie>
2022-09-02 14:35:45 +0100

    First Commit

---
+++ ObjCForSwiftDevelopers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCForSwiftDevelopers
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 02/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // the "@" symbol indicates "this next bit is objective-C"
+        // without the "@"
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
